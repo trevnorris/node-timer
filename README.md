ede5c9da5b4b3afac13c41b5b7d27eda33b091e9
Trevor Norris <trev.norris@gmail.com>
2013-01-29 16:31:50 -0800

    initial commit
    Basic library and information.

---
+++ README.md
@@ -0,0 +1,22 @@
+bench-timer
+===========
+### Create benchmarks for node
+
+Use this to create extensive benchmarks for your libraries. Focus during
+development has been to remove impact on your benchmarks and create an easily
+extendable api for customization.
+
+Installation
+------------
+
+Install with `npm`:
+
+``` bash
+$ npm install bench-timer
+```
+
+Examples
+--------
+
+Currently all usage is in the head of `bench-timer.js`. This will change when I
+find time to create proper documentation.
---
+++ test/async-test.js
@@ -0,0 +1,35 @@
+var timer = require('../bench-timer');
+
+var test0 = timer('test0');
+
+test0.complete(function() {
+  console.log('test0', arguments);
+  test2.start();
+});
+
+test0.start();
+
+var test1 = timer('test1',1000,5);
+
+test1.complete(function() {
+  console.log('test1', arguments);
+  test0.end();
+});
+
+test1.start();
+
+setInterval(function() {
+  test1.inc();
+}, 20);
+
+var test2 = timer('test2', 1000, 5, true);
+
+test2.start(function() {
+  setInterval(function() {
+    test2.inc();
+  }, 10);
+});
+
+test2.complete(function() {
+  console.log('test2', arguments);
+});
