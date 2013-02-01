3dc5f119352c5b13084a154fda95a492d1b698de
Trevor Norris <trev.norris@gmail.com>
2013-01-31 23:31:03 -0800

    simplify numeric paramater parsing
    In latest v8, strings that start with '0' are no longer treated as
    octals. So also going to drop support.
    
    See: http://code.google.com/p/v8/issues/detail?id=1645

--- test/test-params.js
+++ test/test-params.js
@@ -8,7 +8,7 @@ var assert = require('assert');
 var timer = require('../lib/bench-timer');
 var params = timer.parse(process.argv);
 var expected = {
-                octal: 0666,
+                octal: 456,
                 float: 1e3,
                 decimal: 1.3,
                 dual: [1, 2],
